8784c49dfb8490abd2f73bb215c93d87dc450016
xEnt22 <xEnt22@ccbf80fc-c172-11de-bb5c-85734917f5ce>
2009-10-26 11:26:54 +0000

    added walking & updating

--- trunk/Jonquer/src/jonquer/packethandler/WalkHandler.java
+++ trunk/Jonquer/src/jonquer/packethandler/WalkHandler.java
@@ -8,7 +8,7 @@ import jonquer.model.World;
 import jonquer.util.Formula;
 
 public class WalkHandler implements PacketHandler {
-   
+
     @Override
     public int getPacketID() {
 	return 1005;
@@ -18,63 +18,73 @@ public class WalkHandler implements PacketHandler {
     public void handlePacket(Player player, byte[] packet) {
 	ByteBuffer bb = ByteBuffer.wrap(packet);
 	bb.order(ByteOrder.LITTLE_ENDIAN);
+
 	
-	// taken from LOTF.
 	byte AddX = 0;
-        byte AddY = 0;
-        byte Dir = (byte)(bb.getShort(8) % 8);
-	
+	byte AddY = 0;
+	byte Dir = (byte)(bb.getShort(8) % 8);
+
 	switch (Dir)
-        {
-            case 0:
-                {
-                    AddY = 1;
-                    break;
-                }
-            case 1:
-                {
-                    AddX = -1;
-                    AddY = 1;
-                    break;
-                }
-            case 2:
-                {
-                    AddX = -1;
-                    break;
-                }
-            case 3:
-                {
-                    AddX = -1;
-                    AddY = -1;
-                    break;
-                }
-            case 4:
-                {
-                    AddY = -1;
-                    break;
-                }
-            case 5:
-                {
-                    AddX = 1;
-                    AddY = -1;
-                    break;
-                }
-            case 6:
-                {
-                    AddX = 1;
-                    break;
-                }
-            case 7:
-                {
-                    AddY = 1;
-                    AddX = 1;
-                    break;
-                }
-        }
+	{
+	case 0:
+	{
+	    AddY = 1;
+	    break;
+	}
+	case 1:
+	{
+	    AddX = -1;
+	    AddY = 1;
+	    break;
+	}
+	case 2:
+	{
+	    AddX = -1;
+	    break;
+	}
+	case 3:
+	{
+	    AddX = -1;
+	    AddY = -1;
+	    break;
+	}
+	case 4:
+	{
+	    AddY = -1;
+	    break;
+	}
+	case 5:
+	{
+	    AddX = 1;
+	    AddY = -1;
+	    break;
+	}
+	case 6:
+	{
+	    AddX = 1;
+	    break;
+	}
+	case 7:
+	{
+	    AddY = 1;
+	    AddX = 1;
+	    break;
+	}
+	}
 	player.getCharacter().setDirection(Dir);
 	player.getCharacter().setX((short)(player.getCharacter().getX() + AddX));
 	player.getCharacter().setY((short)(player.getCharacter().getY() + AddY));
 	
+	for(Player p : World.getWorld().getPlayers()) {
+	    if(p != player) {
+		if(p.getCharacter().getMap() == player.getCharacter().getMap()) {
+		    if(Formula.inView(player.getCharacter(), p.getCharacter())) {
+			p.getActionSender().write(bb);
+		    }
+		}
+	    }
+	}
+
     }
 
 }
